8bdcd713b42dd49ba8870c02c1a0fc943fcdea77
Krasnykh Milena <krasnyxmilena@mail.ru>
2022-09-19 18:04:37 +0300

    commit number 4
    added second method

--- src/metods.java
+++ src/metods.java
@@ -3,4 +3,8 @@ public class metods {
         String regular = "^([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])$";
         return sentence.matches(regular);
     }
+    public static boolean Task2(string sentence){
+        String regular = "^([0-9A-Fa-f]{8})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{12})$";
+        return sentence.matches(regular);
+    }
 }
