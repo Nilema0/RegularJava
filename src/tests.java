e870e40c7821be8981c2393035fe1d4851683644
Krasnykh Milena <krasnyxmilena@mail.ru>
2022-09-19 17:02:50 +0300

    third commit
    added 6 test

--- src/tests.java
+++ src/tests.java
@@ -2,9 +2,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class tests {
-    
+
     @Test
     public void firstTest(){
         Assert.assertTrue(metods.Task1("128.45.100.0"));
     }
+    @Test
+    public void 2Test(){
+        Assert.assertTrue(metods.Task1("0.0.0.0"));
+    }
+    @Test
+    public void 3Test(){
+        Assert.assertTrue(metods.Task1("0.13.15.200"));
+    }
+    @Test
+    public void 4Test(){
+        Assert.assertTrue(metods.Task1("240.239.238.237"));
+    }
+    @Test
+    public void 5Test(){
+        Assert.assertTrue(metods.Task1("100.101.102.103"));
+    }
+    @Test
+    public void 6Test(){
+        Assert.assertTrue(metods.Task1("0.1.2.3"));
+    }
+    
 }
