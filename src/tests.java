2fed5bb500033955ebfa5b028826b64e5e0b01c3
Krasnykh Milena <krasnyxmilena@mail.ru>
2022-09-19 17:07:28 +0300

    commit number 3.2
    added correctly 5 tests

--- src/tests.java
+++ src/tests.java
@@ -7,25 +7,26 @@ public class tests {
     public void firstTest(){
         Assert.assertTrue(metods.Task1("128.45.100.0"));
     }
+
     @Test
-    public void 2Test(){
+    public void Test2(){
         Assert.assertTrue(metods.Task1("0.0.0.0"));
     }
     @Test
-    public void 3Test(){
+    public void Test3(){
         Assert.assertTrue(metods.Task1("0.13.15.200"));
     }
     @Test
-    public void 4Test(){
+    public void Test4(){
         Assert.assertTrue(metods.Task1("240.239.238.237"));
     }
     @Test
-    public void 5Test(){
+    public void Test5(){
         Assert.assertTrue(metods.Task1("100.101.102.103"));
     }
     @Test
-    public void 6Test(){
+    public void Test6(){
         Assert.assertTrue(metods.Task1("0.1.2.3"));
     }
-    
+
 }
