5fe2ecd316803ec0b506125844ff5bb6918bdc72
Krasnyx Milena <krasnyxmilena@mail.ru>
2022-10-08 15:45:28 +0300

    Totally last commit
    Gave new nfme to the methods and tests

--- src/metods.java
+++ src/metods.java
@@ -1,17 +1,17 @@
 public class metods {
-    public static boolean Task1(String sentence){
+    public static boolean CheckIp(String sentence){
         String regular = "^([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])\\.([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])$";
         return sentence.matches(regular);
     }
-    public static boolean Task2(String sentence){
+    public static boolean CheckGuid(String sentence){
         String regular = "^([0-9A-Fa-f]{8})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{4})-([0-9A-Fa-f]{12})$";
         return sentence.matches(regular);
     }
-    public static boolean Task3(String sentence){
+    public static boolean CheckUrl(String sentence){
         String regular = "^((https?)?:\\/\\/)?([0-9a-zA-Z][0-9a-zA-Z-]{0,63}[0-9a-zA-Z]\\.)+(ru|com|net)(:\\d+)?((\\/[0-9a-zA-Z][0-9a-zA-Z-]{0,63}[0-9a-zA-Z])+)?(\\/[a-zA-Z]+\\?(\\w+=\\w+)(&\\w+=\\w+)*)?(#\\w+)?$";
         return  sentence.matches(regular);
     }
-    public static boolean Task4(String sentence){
+    public static boolean CheckPassword(String sentence){
         String regular = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[\\w]{8,}$";
         return sentence.matches(regular);
     }
--- src/tests.java
+++ src/tests.java
@@ -4,205 +4,207 @@ import org.junit.Test;
 public class tests {
 
     @Test
-    public void firstTest(){
-        Assert.assertTrue(metods.Task1("128.45.100.0"));
+    public void CorrectTestIp1(){
+        Assert.assertTrue(metods.CheckIp("128.45.100.0"));
     }
 
     @Test
-    public void Test2(){
-        Assert.assertTrue(metods.Task1("0.0.0.0"));
+    public void CorrectTestIp2(){
+        Assert.assertTrue(metods.CheckIp("0.0.0.0"));
     }
     @Test
-    public void Test3(){
-        Assert.assertTrue(metods.Task1("0.13.15.200"));
+    public void CorrectTestIp3(){
+        Assert.assertTrue(metods.CheckIp("0.13.15.200"));
     }
     @Test
-    public void Test4(){
-        Assert.assertTrue(metods.Task1("240.239.238.237"));
+    public void CorrectTestIp4(){
+        Assert.assertTrue(metods.CheckIp("240.239.238.237"));
     }
     @Test
-    public void Test5(){
-        Assert.assertTrue(metods.Task1("100.101.102.103"));
+    public void CorrectTestIp5(){
+        Assert.assertTrue(metods.CheckIp("100.101.102.103"));
     }
     @Test
-    public void Test6(){
-        Assert.assertTrue(metods.Task1("0.1.2.3"));
+    public void CorrectTestIp6(){
+        Assert.assertTrue(metods.CheckIp("0.1.2.3"));
     }
 
     @Test
-    public void Test7(){
-        Assert.assertFalse(metods.Task1("256.0.0.0"));
+    public void WrongTestIp1(){
+        Assert.assertFalse(metods.CheckIp("256.0.0.0"));
     }
     @Test
-    public void Test8(){
-        Assert.assertFalse(metods.Task1("a.123.0.0"));
+    public void WrongTestIp2(){
+        Assert.assertFalse(metods.CheckIp("a.123.0.0"));
     }
     @Test
-    public void Test9(){
-        Assert.assertFalse(metods.Task1("0.12.134.5."));
+    public void WrongTestIp3(){
+        Assert.assertFalse(metods.CheckIp("0.12.134.5."));
     }
     @Test
-    public void Test10(){
-        Assert.assertFalse(metods.Task1("abc.def.ghi.jkl"));
+    public void WrongTestIp4(){
+
+        Assert.assertFalse(metods.CheckIp("abc.def.ghi.jkl"));
     }
     @Test
-    public void Test11(){
-        Assert.assertFalse(metods.Task1("256.255.254.253"));
+    public void WrongTestIp5(){
+
+        Assert.assertFalse(metods.CheckIp("256.255.254.253"));
     }
     @Test
-    public void Test12(){
-        Assert.assertFalse(metods.Task1("12.13.14"));
+    public void WrongTestIp6(){
+        Assert.assertFalse(metods.CheckIp("12.13.14"));
     }
     @Test
-    public void Test13(){
-        Assert.assertTrue(metods.Task2("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
+    public void CorrectTestGUID1(){
+        Assert.assertTrue(metods.CheckGuid("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
     }
     @Test
-    public void Test14(){
-        Assert.assertTrue(metods.Task2("002fd0e4-00fd-090A-ca30-0d00a0038ba0"));
+    public void CorrectTestGUID2(){
+        Assert.assertTrue(metods.CheckGuid("002fd0e4-00fd-090A-ca30-0d00a0038ba0"));
     }
     @Test
-    public void Test15(){
-        Assert.assertTrue(metods.Task2("e02fd0e4-00fd-AA0A-ca30-0d00a0038ba0"));
+    public void CorrectTestGUID3(){
+        Assert.assertTrue(metods.CheckGuid("e02fd0e4-00fd-AA0A-ca30-0d00a0038ba0"));
     }
     @Test
-    public void Test16(){
-        Assert.assertTrue(metods.Task2("00000000-1111-2222-3333-444444444444"));
+    public void CorrectTestGUID4(){
+        Assert.assertTrue(metods.CheckGuid("00000000-1111-2222-3333-444444444444"));
     }
     @Test
-    public void Test17(){
-        Assert.assertTrue(metods.Task2("AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"));
+    public void CorrectTestGUID5(){
+        Assert.assertTrue(metods.CheckGuid("AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"));
     }
     @Test
-    public void Test18(){
-        Assert.assertTrue(metods.Task2("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"));
+    public void CorrectTestGUID6(){
+        Assert.assertTrue(metods.CheckGuid("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"));
     }
     @Test
-    public void Test19(){
-        Assert.assertFalse(metods.Task2("e02fd0e400fd090Aca300d00a0038ba0"));
+    public void WrongTestGUID1(){
+        Assert.assertFalse(metods.CheckGuid("e02fd0e400fd090Aca300d00a0038ba0"));
     }
     @Test
-    public void Test20(){
-        Assert.assertFalse(metods.Task2("e02fd0e-400fd090Aca300d00a0038ba0"));
+    public void WrongTestGUID2(){
+        Assert.assertFalse(metods.CheckGuid("e02fd0e-400fd090Aca300d00a0038ba0"));
     }
     @Test
-    public void Test21(){
-        Assert.assertFalse(metods.Task2("e02fd0e4-00fo-090A-ca30-0d00a0038ba0"));
+    public void WrongTestGUID3(){
+        Assert.assertFalse(metods.CheckGuid("e02fd0e4-00fo-090A-ca30-0d00a0038ba0"));
     }
     @Test
-    public void Test22(){
-        Assert.assertFalse(metods.Task2("e02fd0e4.00fd.090A.ca30.0d00a0038ba0"));
+    public void WrongTestGUID4(){
+        Assert.assertFalse(metods.CheckGuid("e02fd0e4.00fd.090A.ca30.0d00a0038ba0"));
     }
     @Test
-    public void Test23(){
-        Assert.assertFalse(metods.Task2("0d00a0038ba0"));
+    public void WrongTestGUID5(){
+        Assert.assertFalse(metods.CheckGuid("0d00a0038ba0"));
     }
     @Test
-    public void Test24(){
-        Assert.assertFalse(metods.Task2("e02fd0e400fd-090A-ca30-0d00-a0038ba0"));
+    public void WrongTestGUID6(){
+        Assert.assertFalse(metods.CheckGuid("e02fd0e400fd-090A-ca30-0d00-a0038ba0"));
     }
     @Test
-    public void Test25(){
-        Assert.assertTrue(metods.Task3("http://www.example.com"));
+    public void CorrectTestURL1(){
+        Assert.assertTrue(metods.CheckUrl("http://www.example.com"));
     }
     @Test
-    public void Test26(){
-        Assert.assertTrue(metods.Task3("https://example.com"));
+    public void CorrectTestURL2(){
+        Assert.assertTrue(metods.CheckUrl("https://example.com"));
     }
     @Test
-    public void Test27(){
-        Assert.assertTrue(metods.Task3("amanda.com"));
+    public void CorrectTestURL3(){
+        Assert.assertTrue(metods.CheckUrl("amanda.com"));
     }
     @Test
-    public void Test28(){
-        Assert.assertTrue(metods.Task3("so-me.com"));
+    public void CorrectTestURL4(){
+        Assert.assertTrue(metods.CheckUrl("so-me.com"));
     }
     @Test
-    public void Test29(){
-        Assert.assertTrue(metods.Task3("https://www.google.com/search?q=a2wd&sxsrf=ALiCzsZagi"));
+    public void CorrectTestURL5(){
+        Assert.assertTrue(metods.CheckUrl("https://www.google.com/search?q=a2wd&sxsrf=ALiCzsZagi"));
     }
     @Test
-    public void Test30(){
-        Assert.assertTrue(metods.Task3("port.com:1"));
+    public void CorrectTestURL6(){
+        Assert.assertTrue(metods.CheckUrl("port.com:1"));
     }
     @Test
-    public void Test31(){
-        Assert.assertTrue(metods.Task3("www.sait.com/papka#yakor"));
+    public void CorrectTestURL7(){
+        Assert.assertTrue(metods.CheckUrl("www.sait.com/papka#yakor"));
     }
     @Test
-    public void Test32(){
-        Assert.assertFalse(metods.Task3("Just Text, http://a.com"));
+    public void WrongTestURL1(){
+        Assert.assertFalse(metods.CheckUrl("Just Text, http://a.com"));
     }
     @Test
-    public void Test33(){
-        Assert.assertFalse(metods.Task3("http://a.com"));
+    public void WrongTestURL2(){
+        Assert.assertFalse(metods.CheckUrl("http://a.com"));
     }
     @Test
-    public void Test34(){
-        Assert.assertFalse(metods.Task3("http://-aa.com"));
+    public void WrongTestURL3(){
+        Assert.assertFalse(metods.CheckUrl("http://-aa.com"));
     }
     @Test
-    public void Test35(){
-        Assert.assertFalse(metods.Task3("http://a.com:"));
+    public void WrongTestURL4(){
+        Assert.assertFalse(metods.CheckUrl("http://a.com:"));
     }
     @Test
-    public void Test36(){
-        Assert.assertFalse(metods.Task3("www.sait.com/papka#"));
+    public void WrongTestURL5(){
+        Assert.assertFalse(metods.CheckUrl("www.sait.com/papka#"));
     }
     @Test
-    public void Test37(){
-        Assert.assertFalse(metods.Task3("www.sait.com/papka?"));
+    public void WrongTestURL6(){
+        Assert.assertFalse(metods.CheckUrl("www.sait.com/papka?"));
     }
     @Test
-    public void Test38(){
-        Assert.assertFalse(metods.Task3("www.sait"));
+    public void WrongTestURL7(){
+        Assert.assertFalse(metods.CheckUrl("www.sait"));
     }
     @Test
-    public void Test39(){
-        Assert.assertTrue(metods.Task4("C00l_Pass"));
+    public void CorrectTestPassword1(){
+        Assert.assertTrue(metods.CheckPassword("C00l_Pass"));
     }
     @Test
-    public void Test40(){
-        Assert.assertTrue(metods.Task4("C00l_Pass23"));
+    public void CorrectTestPassword2(){
+        Assert.assertTrue(metods.CheckPassword("C00l_Pass23"));
     }
     @Test
-    public void Test41(){
-        Assert.assertTrue(metods.Task4("new_C00l_Pass"));
+    public void CorrectTestPassword3(){
+        Assert.assertTrue(metods.CheckPassword("new_C00l_Pass"));
     }
     @Test
-    public void Test42(){
-        Assert.assertTrue(metods.Task4("Best_C00l_Pass"));
+    public void CorrectTestPassword4(){
+        Assert.assertTrue(metods.CheckPassword("Best_C00l_Pass"));
     }
     @Test
-    public void Test43(){
-        Assert.assertTrue(metods.Task4("SuperPas1"));
+    public void CorrectTestPassword5(){
+        Assert.assertTrue(metods.CheckPassword("SuperPas1"));
     }
     @Test
-    public void Test44(){
-        Assert.assertTrue(metods.Task4("SuperPas1_forever"));
+    public void CorrectTestPassword6(){
+        Assert.assertTrue(metods.CheckPassword("SuperPas1_forever"));
     }
     @Test
-    public void Test45(){
-        Assert.assertFalse(metods.Task4("Superpas"));
+    public void WrongTestPassword1(){
+        Assert.assertFalse(metods.CheckPassword("Superpas"));
     }
     @Test
-    public void Test46(){
-        Assert.assertFalse(metods.Task4("superpas1"));
+    public void WrongTestPassword2(){
+        Assert.assertFalse(metods.CheckPassword("superpas1"));
     }
     @Test
-    public void Test47(){
-        Assert.assertFalse(metods.Task4("SUPERPASS1"));
+    public void WrongTestPassword3(){
+        Assert.assertFalse(metods.CheckPassword("SUPERPASS1"));
     }
     @Test
-    public void Test48(){
-        Assert.assertFalse(metods.Task4("Super1"));
+    public void WrongTestPassword4(){
+        Assert.assertFalse(metods.CheckPassword("Super1"));
     }
     @Test
-    public void Test49(){
-        Assert.assertFalse(metods.Task4("COO1"));
+    public void WrongTestPassword5(){
+        Assert.assertFalse(metods.CheckPassword("COO1"));
     }
     @Test
-    public void Test50(){
-        Assert.assertFalse(metods.Task4("Coll-password"));
+    public void WrongTestPassword6(){
+        Assert.assertFalse(metods.CheckPassword("Coll-password"));
     }
 }
