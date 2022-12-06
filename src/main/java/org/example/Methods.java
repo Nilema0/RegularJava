8022e60dd02d4c09396adf52f22acbcdf1160a44
Krasnyx Milena <krasnyxmilena@mail.ru>
2022-12-06 16:19:57 +0300

    Была изменена библиотека для логирования.

--- src/main/java/org/example/Methods.java
+++ src/main/java/org/example/Methods.java
@@ -1,15 +1,14 @@
 package org.example;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 
 /**
  * Методы, проверяющие подходит ли введённая строка под шаблоны IP, GUID, URL
  * или удовлетвряет сложности для пароля.
  */
+@Log4j2
 public class Methods {
-
-    private static final Logger LOG = Logger.getLogger(Methods.class);
-
     /**
      * проверяет введённую строку на соответствие шаблону IP
      *
@@ -17,7 +16,7 @@ public class Methods {
      * @return возвращает true, если входная строка соответсвует шаблону IP, иначе возвращает false
      */
     public static boolean checkIp(final String sentence) {
-        LOG.info("Начался метод проверки IP. Входная строка: " + sentence);
+        log.log(Level.INFO, "Начался метод проверки IP. Входная строка: " + sentence);
         final String part = "([1-9]\\d|[0-9]|25[0-5]|1[0-9][0-9]|2[0-4][0-9])";
         return sentence.matches(part + "\\." + part + "\\." + part + "\\." + part + "$");
     }
@@ -29,7 +28,7 @@ public class Methods {
      * @return возвращает true, если входная строка соответсвует шаблону GUID, иначе возвращает false
      */
     public static boolean checkGuid(final String sentence) {
-        LOG.info("Начался метод проверки GUID. Входная строка: " + sentence);
+        log.log(Level.INFO, "Начался метод проверки GUID. Входная строка: " + sentence);
         return sentence.matches("^([0-9A-Fa-f]{8})-([0-9A-Fa-f]{4}-){3}([0-9A-Fa-f]{12})$");
     }
 
@@ -40,7 +39,7 @@ public class Methods {
      * @return возвращает true, если входная строка соответсвует шаблону URL, иначе возвращает false
      */
     public static boolean checkUrl(final String sentence) {
-        LOG.info("Начался метод проверки URL. Входная строка: " + sentence);
+        log.log(Level.INFO, "Начался метод проверки URL. Входная строка: " + sentence);
         return sentence.matches(
                 "^((https?)?:\\/\\/)?" +
                 "([0-9a-zA-Z][0-9a-zA-Z-]{0,63}" +
@@ -59,7 +58,7 @@ public class Methods {
      * @return возвращает true, если входная строка соответсвует сложности пароля, иначе возвращает false
      */
     public static boolean checkPassword(final String sentence) {
-        LOG.info("Начался метод проверки пароля. Входная строка: " + sentence);
+        log.log(Level.INFO, "Начался метод проверки пароля. Входная строка: " + sentence);
         return sentence.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\w]{8,}$");
     }
 }
