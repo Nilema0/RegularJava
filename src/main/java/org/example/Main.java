a6154b25bd23e2291ab2ffa9cf12a710a13fa0a3
Krasnyx Milena <krasnyxmilena@mail.ru>
2022-12-06 12:43:59 +0300

    Добавлен метод generateMessage, в который поместилась конструкция switch.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,7 @@
 package org.example;
-
 import lombok.val;
-
 import java.util.Scanner;
-
 import static org.example.Methods.*;
-
 
 public class Main {
     public static void main(String[] args) throws java.io.IOException {
@@ -17,42 +13,46 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         val choice = in.nextLine();
+
         if ((choice.matches("[1-4]"))) {
 
             System.out.print("Введите строку: ");
             val line = in.nextLine();
 
-            switch (choice) {
-                case "1":
-                    if (checkIp(line)) {
-                        System.out.println("Строка IP подходит под шаблон!");
-                    } else {
-                        System.out.println("Строка IP не подходит под шаблон!");
-                    }
-                    break;
-                case "2":
-                    if (checkGuid(line)) {
-                        System.out.println("Строка GUID подходит под шаблон!");
-                    } else {
-                        System.out.println("Строка GUID не подходит под шаблон!");
-                    }
-                    break;
-                case "3":
-                    if (checkUrl(line)) {
-                        System.out.println("Строка URL подходит под шаблон!");
-                    } else {
-                        System.out.println("Строка URL не подходит под шаблон!");
-                    }
-                    break;
-                case "4":
-                    if (checkPassword(line)) {
-                        System.out.println("Пароль подходит под шаблон!");
-                    } else {
-                        System.out.println("Пароль не подходит под шаблон!");
-                    }
-                    break;
-            }
+            System.out.println(generateMessage(choice, line));
+
         }
-        else System.out.println("Введите целое число в диапазоне от 1 до 4. ");
+        else System.out.println("Необходимо ввести целое число в диапазоне от 1 до 4. ");
+    }
+
+    public static String generateMessage(final String choice, final String line){
+        switch (choice) {
+            case "1":
+                if (checkIp(line)) {
+                    return("Строка IP подходит под шаблон!");
+                } else {
+                    return("Строка IP не подходит под шаблон!");
+                }
+            case "2":
+                if (checkGuid(line)) {
+                    return("Строка GUID подходит под шаблон!");
+                } else {
+                    return("Строка GUID не подходит под шаблон!");
+                }
+            case "3":
+                if (checkUrl(line)) {
+                    return("Строка URL подходит под шаблон!");
+                } else {
+                    return("Строка URL не подходит под шаблон!");
+                }
+            case "4":
+                if (checkPassword(line)) {
+                    return("Пароль подходит под шаблон!");
+                } else {
+                    return("Пароль не подходит под шаблон!");
+                }
+            default:
+                return("something");
+        }
     }
 }
