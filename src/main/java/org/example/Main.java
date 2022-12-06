bb97eee728f48d2184daea974195417b385378e8
Krasnyx Milena <krasnyxmilena@mail.ru>
2022-12-06 12:17:21 +0300

    Была добавлена и использована библиотека lombok. Добавила обработку ввода данных.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import lombok.val;
+
 import java.util.Scanner;
+
 import static org.example.Methods.*;
 
 
@@ -13,43 +16,43 @@ public class Main {
                 "4. Проверить пароль. ");
 
         Scanner in = new Scanner(System.in);
-        int choice = Integer.parseInt(in.nextLine());
-        System.out.print("Введите строку: ");
-        String line = in.nextLine();
+        val choice = in.nextLine();
+        if ((choice.matches("[1-4]"))) {
 
-        switch (choice) {
-            case 1:
-                if (checkIp(line)) {
-                    System.out.println("Строка IP подходит под шаблон!");
-                }
-                else {
-                    System.out.println("Строка IP не подходит под шаблон!");
-                }
-                break;
-            case 2:
-                if (checkGuid(line)){
-                    System.out.println("Строка GUID подходит под шаблон!");
-                }
-                else {
-                    System.out.println("Строка GUID не подходит под шаблон!");
-                }
-                break;
-            case 3:
-                if (checkUrl(line)){
-                    System.out.println("Строка URL подходит под шаблон!");
-                }
-                else {
-                    System.out.println("Строка URL не подходит под шаблон!");
-                }
-                break;
-            case 4:
-                if (checkPassword(line)){
-                    System.out.println("Пароль подходит под шаблон!");
-                }
-                else {
-                    System.out.println("Пароль не подходит под шаблон!");
-                }
-                break;
+            System.out.print("Введите строку: ");
+            val line = in.nextLine();
+
+            switch (choice) {
+                case "1":
+                    if (checkIp(line)) {
+                        System.out.println("Строка IP подходит под шаблон!");
+                    } else {
+                        System.out.println("Строка IP не подходит под шаблон!");
+                    }
+                    break;
+                case "2":
+                    if (checkGuid(line)) {
+                        System.out.println("Строка GUID подходит под шаблон!");
+                    } else {
+                        System.out.println("Строка GUID не подходит под шаблон!");
+                    }
+                    break;
+                case "3":
+                    if (checkUrl(line)) {
+                        System.out.println("Строка URL подходит под шаблон!");
+                    } else {
+                        System.out.println("Строка URL не подходит под шаблон!");
+                    }
+                    break;
+                case "4":
+                    if (checkPassword(line)) {
+                        System.out.println("Пароль подходит под шаблон!");
+                    } else {
+                        System.out.println("Пароль не подходит под шаблон!");
+                    }
+                    break;
+            }
         }
+        else System.out.println("Введите целое число в диапазоне от 1 до 4. ");
     }
 }
